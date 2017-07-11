929f0ff87432eb9e000fb2076454873a36b0787e
ecomohr <ecomohr@gmail.com>
2017-07-11 15:12:48 -0800

    username to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nyqfy%%pnosptt^rey$(yqxgsupuuoq49==8^!yk-4&priwl%!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','ecomohr.pythonanywhere.com']
 
 
 # Application definition
